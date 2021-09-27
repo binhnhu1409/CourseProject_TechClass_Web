7d816a8dcea6bffd80146497a584d92713e95bbc
binhnhu1409 <binhnhu1409@gmail.com>
2021-09-27 14:57:44 +0300

    working on gallery page, done a part of it. I'm stuck with the slide show (JS file) of 2nd set of images, so I decide to save current code here just in case I mess the whole gallery page

--- JS/final_project_javascript.js
+++ JS/final_project_javascript.js
@@ -1,7 +1,6 @@
-// Automatic Slideshow - change image every 3 seconds
+// Automatic Slideshow - change image every 3 seconds - homepage
 var myIndex = 0;
 change_image();
-
 function change_image() {
   var i;
   var x = document.getElementsByClassName("image_to_change");
@@ -13,6 +12,22 @@ function change_image() {
   x[myIndex-1].style.display = "block";
   setTimeout (change_image, 3000);
 }
+
 //show current date and time at the bottom of homepage
 var show_time = new Date();
 document.getElementById("show_date_time").innerHTML = show_time;
+
+// Automatic Slideshow - change image every 3 seconds - gallery nature
+var slide_index = 0;
+slideshow();
+function slideshow() {
+  var j;
+  var y = document.getElementsByClassName("slideshow_nature");
+  for (j = 0; j<y.length; j++) {
+    y[j].style.display = "none";
+  }
+  slide_index++;
+  if (slide_index > y.length) {slide_index = 1}
+  y[slide_index-1].style.display = "block";
+  setTimeout (slideshow, 3000);
+}
