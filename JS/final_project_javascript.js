9c31ee8f70c18509723245e9278ac7235756a254
binhnhu1409 <binhnhu1409@gmail.com>
2021-09-28 15:27:00 +0300

    done HTML of Skill page.

--- JS/final_project_javascript.js
+++ JS/final_project_javascript.js
@@ -16,21 +16,3 @@ function change_image() {
 //show current date and time at the bottom of homepage
 var show_time = new Date();
 document.getElementById("show_date_time").innerHTML = show_time;
-
-// JS of project page
-function bigger(project_image) {
-  project_image.style.height = "450px";
-  project_image.style.width = "800px";
-}
-function back_normal(project_image) {
-  project_image.style.height = "400px";
-  project_image.style.width = "600px";
-}
-function loaded () {
-  window.alert ("Project page is loading.");
-}
-var scrolled_times = 0;
-function counting_scroll() {
-  console.log(scrolled_times);
-  document.getElementById("scroll-time").innerHTML = scrolled_times += 1;
-}
