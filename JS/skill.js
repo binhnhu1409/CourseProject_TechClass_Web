d0af3c5a74c2d896030392beda82ca44fbbab211
binhnhu1409 <binhnhu1409@gmail.com>
2021-09-30 12:09:50 +0300

    SKillPage: adding skill to the first of the list function ok

--- JS/skill.js
+++ JS/skill.js
@@ -1,11 +1,12 @@
 var counting_skill = document.getElementsByClassName("ListItem ");
 document.getElementById("click_to_add").onclick = function() {skillFunction()};
+// to add the first skill
+skill_name.oninput = function () {
+  new_skill_here.innerHTML = skill_name.value;
+};
+//skill_photo.oninput = function ()
+  //new_photo_here.innerHTML = skill_photo.value;
+//}
 function skillFunction() {
-  //var new_skill_name = document.forms["SkillForm"];???
   counting_skill[counting_skill.length-1].remove(); // to remove the last_skill from the list
-   //var new_skill_name = document.getElementById("skill_name");
-   //document.getElementById("new_skill_here").innerHTML = new_skill_name;
-  //document.getElementById("new_skill_here").innerHTML = new_skill_name;
-   //var new_skill_photo =  document.getElementById("skill_photo");
-
 }
